bf73331ea84b7a0fe30b020aab07bd630c90cfb9
kchouk1 <107952217+kchouk1@users.noreply.github.com>
2025-08-03 20:31:57 +0300

    Update node.js

--- node/node.js
+++ node/node.js
@@ -1,1 +1,26 @@
+const express = require("express");
+const UserService = require("./services/user.service");
+const ErrorHandler = require("./middleware/error.middleware");
+
+const router = express.Router();
+const userService = new UserService();
+
+router.get("/user/:id", ErrorHandler.asyncWrapper(async (req, res) => {
+  const user = await userService.getUserById(req.params.id);
+  res.json(user);
+}));
+
+router.put("/user/:id", ErrorHandler.asyncWrapper(async (req, res) => {
+  await userService.updateUser(req.params.id, req.body);
+  res.json({ message: "User updated successfully" });
+}));
+
+router.post("/user/change-password", ErrorHandler.asyncWrapper(async (req, res) => {
+  await userService.changePassword(req.body);
+  res.json({ message: "Password changed successfully" });
+}));
+
+router.use(ErrorHandler.handle);
+
+module.exports = router;
 
