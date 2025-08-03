fe35992cc83a24d1c388d301da9a264fc1ef98f3
kchouk1 <107952217+kchouk1@users.noreply.github.com>
2025-08-03 20:28:55 +0300

    Update user.interface.ts

--- angular/interfaces/user.interface.ts
+++ angular/interfaces/user.interface.ts
@@ -1,1 +1,15 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
 
+export interface UpdateUserRequest {
+  name: string;
+  email: string;
+}
+
+export interface ChangePasswordRequest {
+  id: number;
+  password: string;
+}
