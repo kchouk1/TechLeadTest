0a18d7eb16374cd6add62741780b1ccbc08ca386
kchouk1 <107952217+kchouk1@users.noreply.github.com>
2025-08-03 20:39:05 +0300

    Update user.types.js

--- node/types/user.types.js
+++ node/types/user.types.js
@@ -1,1 +1,21 @@
+/**
+ * @typedef {Object} User
+ * @property {number} id
+ * @property {string} name
+ * @property {string} email
+ * @property {string} password
+ */
 
+/**
+ * @typedef {Object} UpdateUserRequest
+ * @property {string} name
+ * @property {string} email
+ */
+
+/**
+ * @typedef {Object} ChangePasswordRequest
+ * @property {number} id
+ * @property {string} password
+ */
+
+module.exports = {};
